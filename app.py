89d2cca78b0ebf9a9001e677c3551eac58f66a89
Realex78 <36749154+Realex78@users.noreply.github.com>
2025-08-14 14:18:38 -0600

    small change

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def index():
 	return jsonify({"message": "something unique"})
 
+# Make sure to use comparison ==
 if __name__ == "__main__":
 	app.run(threaded=True, host='0.0.0.0', port=7878)
